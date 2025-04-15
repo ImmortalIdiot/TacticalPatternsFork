6e39eb1143ae776542ef7ef5310f47e5ad843d41
ImmortalIdiot <max.krasulya@yandex.ru>
2025-04-15 20:20:31 +0300

    add demonstration

---
+++ src/main/kotlin/drivers/Main.kt
@@ -0,0 +1,20 @@
+package drivers
+
+import drivers.active_records.DriverFactory
+import drivers.active_records.DriverRepositoryFactory
+import drivers.transaction_scripts.ScheduleTransactionScript
+import java.time.LocalDateTime
+
+val driverRepository = DriverRepositoryFactory.create()
+val scheduleService = ScheduleTransactionScript(driverRepository)
+
+fun main() {
+    val driver = DriverFactory.create("Иван Иванов")
+    driverRepository.add(driver)
+
+    val start = LocalDateTime.now()
+    val end = start.plusHours(6)
+
+    scheduleService.addShiftToDriver(driver.id, start, end)
+    println(scheduleService.getShiftsOnDate(driver.id, start))
+}
